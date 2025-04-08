31f15fef035d9faec99a5272a42ca6331d6d11d0
justjachowicz <83370566+justjachowicz@users.noreply.github.com>
2025-04-08 18:20:40 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Witaj w moim API'
+
+@app.route('/mojastrona')
+def mojastrona():
+    return 'To jest moja strona!'
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args.get('name', '')
+    if name:
+        return f"Hello {name}!"
+    else: 
+        return 'Hello!'
+
+@app.route('/api/v1.0/predict', methods=['GET'])
+def predict():
+    num1 = float(request.args.get('num1', ''))
+    num2 = float(request.args.get('num2', ''))
+    prediction = 0
+    if num1 + num2 > 5.8:
+        prediction = 1\
+
+    outputDict = {
+        'prediction': prediction,
+        'features': {
+            'num1': num1,
+            'num2': num2
+        }
+    }
+    return outputDict
+    
+
+if __name__ == '__main__':
+    app.run()
